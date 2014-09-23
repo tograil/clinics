88060900c02f4d254c3a62be0cb7d3ac26f41aa2
Andrey Rubayko <tograilx@gmail.com>
2014-09-23 21:43:09 +0300

    gallery added

---
+++ Clinics2/Controllers/RequestsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Clinics2.Controllers
+{
+    public class RequestsController : Controller
+    {
+        public ActionResult GetGalleryImages()
+        {
+            string filePath = Server.MapPath(Url.Content("~/Photos/Gallery/"));
+
+            var files = Directory.GetFiles(filePath);
+
+            return Json(files.Select(file => new { src = string.Format("photos/gallery/{0}", Path.GetFileName(file)), desc = "image" }));
+        }
+
+    }
+}
